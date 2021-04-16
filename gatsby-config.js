025ba8e6c1dd10ce14cc4fb0db7f159f3296b744
ininit <meininit@gmail.com>
2021-04-16 15:51:18 +0800

    chore: init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ininit`,
+    description: ``,
+    author: `@Ininit`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +28,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
